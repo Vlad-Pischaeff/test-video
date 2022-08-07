9bc8a6545409e4fd29bdf6c6368d99bc9d401473
Vlad-Pischaeff <pischaeff@gmail.com>
2022-08-07 13:15:09 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,24 @@ import s from './App.module.sass';
 import * as UI from './compnts';
 
 function App() {
-    useGetFragmentsQuery('');
+    const { isLoading } = useGetFragmentsQuery('');
 
-    return (
-        <div className={s.mainContainer}>
-            <header className={s.header}>
-                Video Player test
-            </header>
-            <div className={s.centralContainer}>
-                <UI.LeftPanel />
-                <UI.RightPanel />
-            </div>
-            <footer className={s.footer}>
-                Vlad Pischaeff &copy; 2022
-            </footer>
-        </div>
-    );
+    return isLoading
+        ?   <div>Loading...</div>
+        :   (
+                <div className={s.mainContainer}>
+                    <header className={s.header}>
+                        Video Player test
+                    </header>
+                    <div className={s.centralContainer}>
+                        <UI.LeftPanel />
+                        <UI.RightPanel />
+                    </div>
+                    <footer className={s.footer}>
+                        Vlad Pischaeff &copy; 2022
+                    </footer>
+                </div>
+            );
 }
 
 export default App;
